50cd0a89e06e9ffc4c2b4ce7f4a3370fb5880627
MiguelRestrepoL <ith1zdragonz@gmail.com>
2025-09-20 20:26:32 -0500

    testing2!!

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import InicioSesion from "./inicioSesion/login.jsx";
+import Registro from "./registrarse/registro.jsx";
 import "./GlobalCSS1.css";
 
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<InicioSesion />} />
+         <Route path="/registro" element={<Registro />} />
       </Routes>
     </div>
   );
