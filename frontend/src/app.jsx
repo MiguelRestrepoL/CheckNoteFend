8650a3bc6f432adf292ef706052ac5fc18dedbb1
MiguelRestrepoL <ith1zdragonz@gmail.com>
2025-09-20 20:14:45 -0500

    testing 11

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import InicioSesion from "./inicioSesion/login.jsx";
-import Registro from "./registrarse/registro.jsx";
-import Inicio from "./home/inicio.jsx";
-import CrearTarea from "./creartarea/creartarea.jsx";
-import OlvidarPw1 from "./olvidarpw/olvidarpw1.jsx";
-import OlvidarPw2 from "./olvidarpw/olvidarpw2.jsx";
-import Tasks from "./tareas/Tasks.jsx";
+
 
 function App() {
   return (
