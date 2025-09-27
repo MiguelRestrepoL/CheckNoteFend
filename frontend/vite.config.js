59485a8a6996926042dc3c2de562d4c5673a00d4
MiguelRestrepoL <ith1zdragonz@gmail.com>
2025-09-27 00:22:46 -0500

    testign?

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  root: 'src', // ahora la raíz es la carpeta src
   build: {
+    outDir: '../dist', // el build saldrá en frontend/dist
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        login: resolve(__dirname, 'login.html')
+        main: resolve(__dirname, '/index.html'),
+        login: resolve(__dirname, 'src/inicioSesion/login.html'),
+        // aquí puedes seguir agregando más entradas
+        // registro: resolve(__dirname, 'src/registrarse/index.html'),
+        // home: resolve(__dirname, 'src/home/index.html'),
       }
     }
   }
