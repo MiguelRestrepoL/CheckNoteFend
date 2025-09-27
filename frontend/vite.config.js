cc23be8fb698219a7124d88ef81ca32a35e46d38
MiguelRestrepoL <ith1zdragonz@gmail.com>
2025-09-27 03:00:09 -0500

    Perfil :)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
         login: resolve(__dirname, 'login.html'),
         registro: resolve(__dirname, 'registro.html'),
         'olvidar-password1': resolve(__dirname, 'olvidar-password1.html'),
-        home: resolve(__dirname, 'home.html')
+        home: resolve(__dirname, 'home.html'),
+        perfil: resolve(__dirname, 'perfil.html')
       }
     }
   }
