194ce612a4dfa96b796ee699aa9c5ff2510bd6a8
MiguelRestrepoL <ith1zdragonz@gmail.com>
2025-09-26 21:56:36 -0500

    Testing html typeshit

--- frontend/src/inicioSesion/login.jsx
+++ frontend/src/inicioSesion/login.jsx
@@ -1,4 +1,4 @@
-
+/*
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
@@ -175,3 +175,4 @@ export default function InicioSesion() {
     </div>
   );
 }
+  */
