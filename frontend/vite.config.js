45dcfc39c07629f140eefcffbc2fdeef87b8172d
MiguelRestrepoL <ith1zdragonz@gmail.com>
2025-09-27 00:43:05 -0500

    Testinggg

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: '.', // raíz es frontend (donde está index.html)
   build: {
-    outDir: 'dist', // Vercel busca esto
-    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'), // index en la raíz
-        login: resolve(__dirname, 'src/inicioSesion/login.html') // login en src
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html')
       }
     }
   }
